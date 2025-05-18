0250df4c31048235d7ecefdec399e9c43c74c8c6
Szymon <kubiakszymonp@gmail.com>
2025-05-18 18:36:17 +0200

    title

--- ostoja-strona/src/app/layout.tsx
+++ ostoja-strona/src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>Stowarzyszenie Ostoja</head>
       <body className={`${PermanentMarker.variable} ${DMSans.variable}`}>
         {children}
       </body>
