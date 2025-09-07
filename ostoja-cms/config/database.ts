448ca686b83ec15d89001273872d1deb7a23965b
Szymon Kubiak <kubiakszymonp@gmail.com>
2025-09-07 18:09:04 +0200

    defaults

--- ostoja-cms/config/database.ts
+++ ostoja-cms/config/database.ts
@@ -76,7 +76,7 @@ export default ({ env }) => {
           __dirname,
           '..',
           '..',
-          env('DATABASE_FILENAME', '.tmp/data.db')
+          env('DATABASE_FILENAME', 'data/data.db')
         ),
       },
       useNullAsDefault: true,
--- ostoja-cms/config/server.ts
+++ ostoja-cms/config/server.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 2337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['defaultKey1', 'defaultKey2', 'defaultKey3', 'defaultKey4']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
